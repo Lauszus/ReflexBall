5612bd48c9034f4597cf23cffbab0318d433c298
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-06-27 15:30:08 +0200

    Added comments

--- Code/buttons.c
+++ Code/buttons.c
@@ -3,12 +3,12 @@
 #include "buttons.h"
 #include "time.h"
 
-void initButtons() {
+void initButtons() { // Initialize the buttons on the evaluation board
 	PDDD = (1 << 3);
 	PFDD = (1 << 6) | (1 << 7);
 }
 
-unsigned char readButtons() {
+unsigned char readButtons() { // Read the buttons buttons in the following order: bit2: PD3, bit1: PF6 and bit0: PF7
 	unsigned char inD, inF;
 	inD = (~PDIN >> 3) & 0x1;
 	inF = (~PFIN >> 6) & 0x3;
